056b93f04dd4a75d279f8ca44a3696243df53de6
 4lexvav <4lexvav@gmail.com>
2023-02-17 15:12:20 +0200

    updates

--- quicksearch/main.go
+++ quicksearch/main.go
@@ -11,6 +11,9 @@ func main() {
 	nums := []int{5, 3, 4, 1, 8, 9, 2, 11}
 	fmt.Println(nums)
 	fmt.Println(quickSort(nums))
+	fmt.Println()
+
+	nums = []int{5, 3, 4, 1, 8, 9, 2, 11}
 	fmt.Println(nums)
 	fmt.Println(quickSortInplace(nums))
 	fmt.Println(nums)
@@ -21,19 +24,22 @@ func quickSort(nums []int) []int {
 		return nums
 	}
 
-	pivot := rand.Int() % len(nums)
+	//pivot := rand.Int() % len(nums)
+	pivot := 0
 	left := make([]int, 0)
 	right := make([]int, 0)
 
+	//nums = append(nums[:pivot], nums[pivot+1:]...)
+
 	for _, v := range nums[1:] {
-		if v <= pivot {
+		if v <= nums[pivot] {
 			left = append(left, v)
 		} else {
 			right = append(right, v)
 		}
 	}
 
-	left = append(quickSort(left), pivot)
+	left = append(quickSort(left), nums[pivot])
 	return append(left, quickSort(right)...)
 }
 
@@ -43,8 +49,7 @@ func quickSortInplace(nums []int) []int {
 	}
 
 	pivot := rand.Int() % len(nums)
-	left := 0
-	right := len(nums) - 1
+	left, right := 0, len(nums)-1
 
 	// shift pivot to the end
 	nums[pivot], nums[right] = nums[right], nums[pivot]
